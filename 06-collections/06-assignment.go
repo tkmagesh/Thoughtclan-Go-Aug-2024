8f4007c947a1cd87a42ef163f06343d82bb7978a
Magesh Kuppan <tkmagesh77@gmail.com>
2024-08-06 12:51:19 +0530

    assignment

---
+++ 06-collections/06-assignment.go
@@ -0,0 +1,12 @@
+/*
+
+Given the following string
+"Cillum anim esse laborum aliqua nostrud Reprehenderit qui et occaecat ad adipisicing incididunt nulla Amet incididunt eiusmod sint fugiat dolor velit cillum cillum fugiat Mollit dolore aliqua reprehenderit eiusmodConsectetur excepteur cillum reprehenderit sint Lorem cupidatat sint id laboris anim cillum ipsum nostrud anim irure fugiat mollit laboris mollit Eu aliquip ex pariatur excepteur dolor laboris laboris Eiusmod id sunt eu excepteur laboris consectetur deserunt Veniam consectetur aliqua occaecat occaecat elit commodo ex magna deserunt nisi fugiat Lorem sunt ut ad sunt do ullamco consequat velit aute minim exercitation in fugiat"
+
+find out the "size of words" that occurs the most
+ex output:
+4 letter words occurs the most with 12 occurances
+
+Hint:
+use strings.Split() functions
+*/
