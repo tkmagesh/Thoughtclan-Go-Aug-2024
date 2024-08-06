765f2c3d18d3e69026b7a637eb120eb422b4d914
Magesh Kuppan <tkmagesh77@gmail.com>
2024-08-06 15:26:19 +0530

    deferred functions

---
+++ 08-deferred/01-example.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+func main() {
+	defer func() {
+		fmt.Println("	[main] deferred")
+	}()
+	fmt.Println("main started")
+	f1()
+	fmt.Println("main completed")
+}
+
+func f1() {
+	/*
+		defer func() {
+			fmt.Println("	[f1] deferred - 1")
+		}()
+		defer func() {
+			fmt.Println("	[f1] deferred - 2")
+		}()
+		defer func() {
+			fmt.Println("	[f1] deferred - 3")
+		}()
+	*/
+
+	defer fmt.Println("	[f1] deferred - 1")
+	defer fmt.Println("	[f1] deferred - 2")
+	defer fmt.Println("	[f1] deferred - 3")
+	fmt.Println("f1 started")
+	f2()
+	fmt.Println("f1 completed")
+}
+
+func f2() {
+	var x int = 100
+	// defer fmt.Printf("	[f2] deferred - x = %d\n", x)
+	defer func() {
+		fmt.Printf("	[f2] deferred - x = %d\n", x)
+	}()
+	fmt.Println("f2 started")
+	fmt.Println("f2 completed")
+	x = 200
+}
